a313b5960c9a7cd45a0b00e381bebca2f07b86a3
Scott Mathog <smathog@gmail.com>
2021-10-24 23:33:12 -0700

    Finished Rust Acronym

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    phrase.split(|c: char| c.is_ascii_whitespace() || c == '-' || c == '_')
+        .map(|s: &str| s.chars().filter(|c| c.is_alphabetic()).collect::<String>())
+        .filter(|s| !s.is_empty())
+        .map(|s| {
+                let bytes = s.as_bytes();
+                if s.chars().filter(char::is_ascii_uppercase).count() <= 1
+                    || bytes[bytes.len() - 1].is_ascii_uppercase() { //Just take first letter and capitalize
+                    String::from(char::from(bytes[0].to_ascii_uppercase()))
+                } else {
+                    s.chars().filter(char::is_ascii_uppercase).collect::<String>()
+                }
+        })
+        .collect()
 }
