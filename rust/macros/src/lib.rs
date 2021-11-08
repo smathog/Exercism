4a186bf31c37ca62ff288cd08418f2d95fba2e9b
Scott Mathog <smathog@gmail.com>
2021-11-07 20:56:40 -0800

    End of Day commit 11/7/21

---
+++ rust/macros/src/lib.rs
@@ -0,0 +1,13 @@
+#[macro_export]
+macro_rules! hashmap (
+    ($(,)+) => {};
+    ( $($x:expr => $y:expr),* $(,)?) => {
+        {
+            let mut hm = ::std::collections::HashMap::new();
+            $(
+                hm.insert($x, $y);
+            )*
+            hm
+        }
+    };
+);
